6c04f91c5c18c6e577e1e02a558fdce787290311
Nipa <akternipa941@gmail.com>
2022-04-04 19:34:35 +0600

    create component and router

---
+++ src/components/QnA/QnA.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+
+const QnA = () => {
+    return (
+        <div className='container'>
+                <Card>
+  <Card.Header as="h5">What is Context API ?</Card.Header>
+  <Card.Body>
+    <Card.Text>
+      With supporting text below as a natural lead-in to additional content.
+    </Card.Text>
+  </Card.Body>
+</Card>
+        </div>
+    );
+};
+
+export default QnA;
